112ba0b7ab576f3e69ae091a7a265854458562e5
matteoPiccoli <s278725@studenti.polito.it>
2025-05-28 22:47:36 +0200

    feat: add a list for default categories
    What: add a list of pre-defined categories
    Why: to let the user select among the available ones
    Side effects: will be changed in the future if we want to make them editable from the user

---
+++ lib/features/transaction/domain/entities/default_categories.dart
@@ -0,0 +1,47 @@
+import 'package:risparmio/features/transaction/domain/entities/category.dart';
+import 'package:risparmio/features/transaction/domain/entities/transaction.dart';
+
+/// List of default categories. Now are hard-coded, but in the future will be editable from the user.
+
+final defaultCategories = <Category>[
+  Category.create(
+    name: 'Family',
+    type: TransactionType.expense,
+  ),
+  Category.create(
+    name: 'Food',
+    type: TransactionType.expense,
+  ),
+  Category.create(
+    name: "Free Time",
+    type: TransactionType.expense
+  ),
+  Category.create(
+    name: 'Health',
+    type: TransactionType.expense,
+  ),
+  Category.create(
+    name: 'House',
+    type: TransactionType.expense,
+  ),
+  Category.create(
+    name: 'Investing',
+    type: TransactionType.saving,
+  ),
+  Category.create(
+    name: 'Salary',
+    type: TransactionType.income,
+  ),
+  Category.create(
+    name: 'Sports',
+    type: TransactionType.expense,
+  ),
+  Category.create(
+    name: 'Subscriptions',
+    type: TransactionType.expense,
+  ),
+  Category.create(
+    name: 'Transport',
+    type: TransactionType.expense,
+  )
+];
